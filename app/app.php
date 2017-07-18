864f8b14b7096136d22029e7159df10befae0a73
Calla Rudolph and Michelle Poterek <michelle@>
2017-07-18 11:41:45 -0700

    start using silex to create twig files

--- app/app.php
+++ app/app.php
@@ -1,7 +1,9 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/MyClass.php";
+    require_once __DIR__."/../src/Course.php";
+    require_once __DIR__."/../src/Student.php";
+
 
     $server = 'mysql:host=localhost:8889;dbname=university';
     $username = 'root';
@@ -14,7 +16,17 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
-    // routes
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig', array('students' => Student::getAll(), 'courses' => Course::getAll()));
+    });
+
+    $app->get("/students", function() use ($app) {
+        return $app['twig']->render('students.html.twig', array('students' => Student::getAll()));
+    })
+
+    $app->get("/courses", function() use ($app) {
+        return $app['twig']->render('courses.html.twig', array('categories' => Course::getAll()));
+    });
 
     return $app;
 ?>
