5a6613ffcf12b51249188e3ab6a27b55f626e7c9
Calla Rudolph and Michelle Poterek <michelle@>
2017-07-18 13:26:09 -0700

    fill students and courses twigs with successful routes to add objects

--- app/app.php
+++ app/app.php
@@ -22,10 +22,27 @@
 
     $app->get("/students", function() use ($app) {
         return $app['twig']->render('students.html.twig', array('students' => Student::getAll()));
-    })
+    });
+
+    $app->post("/students", function() use ($app) {
+        $name = $_POST['name'];
+        $date = $_POST['date'];
+        $student = new Student($name, $date);
+        $student->save();
+        return $app['twig']->render('students.html.twig', array('students' => Student::getAll()));
+    });
 
     $app->get("/courses", function() use ($app) {
-        return $app['twig']->render('courses.html.twig', array('categories' => Course::getAll()));
+        return $app['twig']->render('courses.html.twig', array('courses' => Course::getAll()));
+    });
+
+
+    $app->post("/courses", function() use ($app) {
+        $course_name = $_POST['course_name'];
+        $number = $_POST['number'];
+        $course = new Course($course_name, $number);
+        $course->save();
+        return $app['twig']->render('courses.html.twig', array('courses' => Course::getAll()));
     });
 
     return $app;
